97bd4cbea3feaa756a0d36a636c0820c125f6b44
miguelbac <mbacebal@gmail.com>
2025-09-25 09:24:43 +0200

    feat : cors implementation

---
+++ src/main/java/org/factoriaf5/config/CorsConfig.java
@@ -0,0 +1,26 @@
+package org.factoriaf5.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import org.springframework.lang.NonNull;
+
+@Configuration
+public class CorsConfig {
+
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(@NonNull CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOriginPatterns("http://localhost:5173")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+            .allowedHeaders("*")
+            .allowCredentials(true);
+      }
+    };
+  }
+}
