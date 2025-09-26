f3c0c0283b2d9a6b2c32dd97602199587a65277d
Jesus Martin <jesus8.mb@gmail.com>
2025-09-26 11:42:20 +0200

    security done

--- src/main/java/org/factoriaf5/security/JpaUserDetailsService.java
+++ src/main/java/org/factoriaf5/security/JpaUserDetailsService.java
@@ -1,5 +1,7 @@
 package org.factoriaf5.security;
 
+import org.factoriaf5.user.User;
+import org.factoriaf5.user.repository.UserRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -8,15 +10,18 @@ import org.springframework.stereotype.Service;
 @Service
 public class JpaUserDetailsService implements UserDetailsService {
 
-  // private UserRepository UserRepository;
+  private final UserRepository userRepository;
 
-  public JpaUserDetailsService() {
+  public JpaUserDetailsService(UserRepository userRepository) {
+    this.userRepository = userRepository;
   }
 
   @Override
-  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'loadUserByUsername'");
+  public UserDetails loadUserByUsername(String dni) throws UsernameNotFoundException {
+    User user = userRepository.findById(dni)
+        .orElseThrow(() -> new UsernameNotFoundException("Usuario con DNI " + dni + " no encontrado"));
+
+    return new SecurityUser(user);
   }
 
 }
